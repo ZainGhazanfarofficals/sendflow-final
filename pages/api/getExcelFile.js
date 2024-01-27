09712967e75b26c6acb756c14992868a44925a05
Zain Ghazanfar <69090613+ZainGhazanfarofficals@users.noreply.github.com>
2024-01-27 22:20:52 +0500

    file changes

--- pages/api/getExcelFile.js
+++ pages/api/getExcelFile.js
@@ -1,35 +1,25 @@
-import fetch from 'node-fetch'; // Import node-fetch to handle HTTP requests
-import ExcelJS from 'exceljs'; // Import ExcelJS for handling Excel files
-import { parse } from 'url'; // Import parse from 'url' module for parsing request URLs
+import fetch from 'node-fetch';
+import ExcelJS from 'exceljs';
 
 export default async (req, res) => {
   try {
-    const { query } = parse(req.url, true);
-    const { filename } = query;
-
+    const { filename } = req.query;
     if (!filename) {
-      res.status(400).json({ message: 'No filename provided.' });
-      return;
+      return res.status(400).json({ message: 'No filename provided.' });
     }
-   console.log(filename);
-    // Construct the Cloudinary URL for the file
+
     const cloudinaryUrl = `https://res.cloudinary.com/dpkkaacjk/raw/upload/uploads/${filename}`;
 
-    // Fetch the file data from Cloudinary
     const response = await fetch(cloudinaryUrl);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const fileData = await response.buffer();
 
-    // Create a new workbook from the file data
     const workbook = new ExcelJS.Workbook();
     await workbook.xlsx.load(fileData);
 
-    // Assuming you want to access the first sheet in the Excel file
     const worksheet = workbook.getWorksheet(1);
-
-    // Convert the worksheet to an array of objects (representing rows)
     const jsonData = [];
     worksheet.eachRow({ includeEmpty: true }, (row, rowNumber) => {
       const rowData = {};
@@ -39,7 +29,6 @@ export default async (req, res) => {
       jsonData.push(rowData);
     });
 
-    // Send the parsed data in the response
     res.status(200).json({ message: 'File retrieved successfully.', data: jsonData });
   } catch (error) {
     console.error('Error retrieving file:', error);
--- pages/api/uploadexcel.js
+++ pages/api/uploadexcel.js
@@ -2,53 +2,37 @@ import multer from 'multer';
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
-// Configure Cloudinary
 cloudinary.config({
   cloud_name: 'dpkkaacjk',
   api_key: '775987195199584',
   api_secret: 'uLSlA65oNFr-jqWLGYmPPVElLT0',
 });
 
-// Define a function to generate the filename
 const generateFilename = (req, file, cb) => {
   const filename = `${Date.now()}-${file.originalname}`;
-  req.generatedFilename = filename; // Store the generated filename in the request object
+  req.generatedFilename = filename;
   cb(null, filename);
 };
 
-// Create a storage engine for multer to use with Cloudinary
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'uploads', // Define your folder name in Cloudinary
-    resource_type: 'raw', // Set the resource type as 'raw' for non-image files
-    public_id: (req, file) => req.generatedFilename, // Use custom public_id (filename)
+    folder: 'uploads',
+    resource_type: 'raw',
+    public_id: (req, file) => req.generatedFilename,
   },
 });
 
-// Create multer instance using the Cloudinary storage engine
 const upload = multer({ storage });
-
-export const config = {
-  api: {
-    bodyParser: false, // Disable default body parser
-  },
-};
-
 const uploadExcelMiddleware = upload.single('ExcelFile');
 
-const uploadExcelAPI = (req, res) => {
+export default async (req, res) => {
   uploadExcelMiddleware(req, res, async (err) => {
     if (err) {
-      console.error(err);  // Log the detailed error
+      console.error(err);
       return res.status(500).json({ error: 'Internal Server Error', details: err.message });
     }
     const filename = req.generatedFilename;
-    console.log(filename);
     res.status(200).json({ success: true, filename });
   });
 };
-
-export default uploadExcelAPI;
-
-// res.status(200).json({ success: true, filename });
